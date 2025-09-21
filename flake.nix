585427725c3f57067ea4302b36ee5504741d220d
Brian-ED <brianellingsgaard9@gmail.com>
2025-09-21 22:51:24 +0200

    nix: nixpkgs: switched from unstable to stable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
   outputs = { self, nixpkgs, flake-utils }: let
     systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" ];
