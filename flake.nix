41e136994b7a44cdf9d202a26a7bf80330dcb9a8
Brian-ED <brianellingsgaard9@gmail.com>
2026-03-15 20:06:26 +0100

    Moving to unstable since dyalog20 is there

--- flake.nix
+++ flake.nix
@@ -3,14 +3,14 @@
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
   outputs = { self, nixpkgs, flake-utils }: let
     systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" ];
   in flake-utils.lib.eachSystem systems (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
-      callPackage = pkgs.lib.callPackageWith ((pkgs.lib.attrsets.unionOfDisjoint pkgs pkgs.xorg) // packages);
+      callPackage = pkgs.lib.callPackageWith (pkgs // packages);
       packages = {
         default = callPackage ./default.nix {};
       };
